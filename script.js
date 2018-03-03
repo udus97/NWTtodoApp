a9de93229b6a004f2cd24684d48cdf7f2bae4213
qudusdev <qudusdev@gmail.com>
2018-03-03 02:01:09 +0100

    Added more comments, commented out a call for a function that has yet to be defined

--- script.js
+++ script.js
@@ -23,3 +23,35 @@ if (!localStorage.NWTTodoList) {
   toDoDatabase = p(localStorage.NWTTodoList);
   recreateDOM(toDoDatabase);
 }
+
+
+
+//Select the input field where users will type their todo task
+let inputField = q('input[type=text]');
+
+//Add an event listener to the input field. When the user types a task and presses the enter key, run this event listener
+inputField.addEventListener('keypress', function (e) {
+  if (e.key === "Enter" || e.which === 13) {
+    // Remove whitespace from what the user enters
+
+    let task = this.value.trim();
+    //if the input is not empty, create a new todo object literal and add it to the todo database
+
+    if (task) {
+      toDoDatabase.push({
+        text: task,
+        done: '',
+        id: toDoDatabase.length + 1,
+        checked: ''
+      });
+
+      //Empty the input back to its initial state
+      this.value = "";
+
+    }
+    localStorage.NWTTodoList = s(toDoDatabase);
+    //This function recreates the view anytime there is a change in the todo database
+    //recreateDOM(toDoDatabase);
+
+  }
+});
